8366b507b5497eccbd7cec9ebb2020bd9ef50ffb
Dewanshu <dewanshu96gmail.com@Dewanshus-MacBook-Pro.local>
2023-09-02 11:33:23 +0530

    Updates

---
+++ common/src/events/base-listner.ts
@@ -0,0 +1,56 @@
+import { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "../enums/subject";
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listner<T extends Event> {
+  private client: Stan;
+  abstract subject: T["subject"];
+  abstract queueGroupName: string;
+  abstract onMessage(data: T["data"], msg: Message): void;
+
+  protected ackWait = 5000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setManualAckMode(true)
+      .setDeliverAllAvailable()
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (msg: Message) => {
+      console.log(
+        `Received event #${msg.getSequence()}, ${this.subject} / ${
+          this.queueGroupName
+        }`
+      );
+
+      const parseData = this.parseMessage(msg);
+      this.onMessage(parseData, msg);
+      msg.ack();
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf8"));
+  }
+}
--- common/src/index.ts
+++ common/src/index.ts
@@ -11,3 +11,8 @@ export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/request-handler";
 export * from "./middlewares/require-auth";
+
+export * from "./events/base-listner";
+export * from "./events/base-publisher";
+export * from "./events/ricket-created-event";
+export * from "./events/subject";
