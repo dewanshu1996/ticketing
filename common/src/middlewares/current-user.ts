787be2a321f89246ed4213cedab5824162c2bab7
Dewanshu <dewanshu96gmail.com@Dewanshus-MacBook-Pro.local>
2023-10-02 00:21:31 +0530

    Updates

--- common/src/middlewares/current-user.ts
+++ common/src/middlewares/current-user.ts
@@ -19,15 +19,15 @@ export const currentUser = (
   res: Response,
   next: NextFunction
 ) => {
-  if (!req.headers.cookie) {
+  let cookie = req.headers.cookie;
+  if (!cookie) {
     return next();
   }
 
+  cookie = cookie!.slice("session=".length);
+
   try {
-    const payload = jwt.verify(
-      req.headers.cookie,
-      process.env.JWT_KEY!
-    ) as UserPayload;
+    const payload = jwt.verify(cookie, process.env.JWT_KEY!) as UserPayload;
     req.currentUser = payload;
   } catch (error) {}
   next();
