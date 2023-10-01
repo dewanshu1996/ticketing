2e56db1899ac5e510d84c9e5fad49773e516f46a
Dewanshu <dewanshu96gmail.com@Dewanshus-MacBook-Pro.local>
2023-10-02 00:08:41 +0530

    Updates

--- common/src/middlewares/current-user.ts
+++ common/src/middlewares/current-user.ts
@@ -19,13 +19,13 @@ export const currentUser = (
   res: Response,
   next: NextFunction
 ) => {
-  if (!req.session?.jwt) {
+  if (!req.cookies?.session) {
     return next();
   }
 
   try {
     const payload = jwt.verify(
-      req.session.jwt,
+      req.cookies.session,
       process.env.JWT_KEY!
     ) as UserPayload;
     req.currentUser = payload;
