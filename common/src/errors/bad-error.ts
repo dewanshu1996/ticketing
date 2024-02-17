7ccd6d8c6afc9e9a6403c8895011c515ff6c5144
Dewanshu <dewanshu96gmail.com@Dewanshus-MacBook-Pro.local>
2024-02-17 21:03:38 +0530

    Updates

--- common/src/errors/bad-error.ts
+++ common/src/errors/bad-error.ts
@@ -4,7 +4,7 @@ export class BadError extends CustomError {
   public statusCode: number = 400;
   constructor(public message: string) {
     super();
-
+    console.log("bad error called");
     Object.setPrototypeOf(this, BadError.prototype);
   }
 
