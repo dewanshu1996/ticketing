c32250afc8b040f23f0deecc9127ed1e2e38c7eb
Dewanshu <dewanshu96gmail.com@Dewanshus-MacBook-Pro.local>
2024-02-18 15:49:21 +0530

    Updates

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -8,6 +8,7 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   console.log("enter error handler");
+  console.log(err);
 
   if (err instanceof CustomError) {
     //   res.status(err.statusCode).send({ errors: err.serializeErrors() });
